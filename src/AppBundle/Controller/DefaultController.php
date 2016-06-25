6b53180c1c6da9ac10b53870afab02acf78aa0cb
Adrian Pietka <adrian@pietka.me>
2016-06-25 22:33:24 +0200

    Cleanup default controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,10 +13,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $product = $this->getDoctrine()
-            ->getRepository('AppBundle:Ticket')
-            ->find(1);
-
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
