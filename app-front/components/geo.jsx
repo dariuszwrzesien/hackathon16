0c45334f269e7f16b8dfba0b2bb1a997415424df
tarnas14 <tarnas14@gmail.com>
2016-06-26 01:00:49 +0200

    looking up provided address to get coordinates from google api

--- app-front/components/geo.jsx
+++ app-front/components/geo.jsx
@@ -1,36 +1,52 @@
 import React from 'react';
 
+import googleMaps from '../services/googleMaps';
+
 const Geo = React.createClass({
     getInitialState () {
+        return {};
+    },
+
+    componentWillMount () {
         const locationEnabled = Boolean('geolocation' in navigator);
 
         if (locationEnabled) {
-            navigator.geolocation.getCurrentPosition(position => {
-                const {latitude, longitude} = position.coords;
-                this.setState({position: {latitude, longitude}});
-            });
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    const {latitude, longitude} = position.coords;
+                    this.setState({position: {latitude, longitude}});
+                }
+            );
         }
+    },
 
-        return {
-            showMap: locationEnabled
-        };
+    showMapOnAddress () {
+        googleMaps.getCoords(this.addressInput.value)
+            .then(
+                coords => this.setState({position: coords, showMap: true}),
+                rejection => console.log('could not get coords for address', rejection)
+            );
     },
 
     renderLocationQuestion () {
-        if (this.state.showMap) {
+        if (this.state.position) {
             return null;
         }
 
         return (
             <div>
                 <p>Nie możemy pobrać lokacji automatycznie, napisz gdzie jesteś:</p>
-                <p>input here</p>
+                <input ref={ref => this.addressInput = ref} type="text" />
+                <button
+                    onClick={this.showMapOnAddress}
+                    type="submit"
+                >Find</button>
             </div>
         );
     },
 
     renderMap () {
-        if (!this.state.showMap) {
+        if (!this.state.position) {
             return null;
         }
 
