691695524e62483b699b573fd4fb29c59cce6ac0
Wrzesien, Dariusz (FP) <dariuszwrzesien@gmail.com>
2016-06-26 01:19:16 +0200

    changed tabs to spaces

--- src/AppBundle/DataFixtures/ORM/LoadTicketData.php
+++ src/AppBundle/DataFixtures/ORM/LoadTicketData.php
@@ -12,15 +12,15 @@ class LoadTicketData implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $ticket = new Ticket();
-		$ticket->setDescription('Lorem ...');
-		$ticket->setCreated(new DateTime());
-		$ticket->setStatus(1);
+        $ticket->setDescription('Lorem ...');
+        $ticket->setCreated(new DateTime());
+        $ticket->setStatus(1);
 
-		$location = new Location();
-		$location->setLatitude(10.0);
-		$location->setLongitude(10.0);
+        $location = new Location();
+        $location->setLatitude(10.0);
+        $location->setLongitude(10.0);
 
-		$ticket->setLocation($location);
+        $ticket->setLocation($location);
 
         $manager->persist($ticket);
         $manager->flush();
--- src/AppBundle/Entity/Location.php
+++ src/AppBundle/Entity/Location.php
@@ -18,19 +18,19 @@ class Location {
      */
     public $longitude;
 
-	public function getLatitude() {
-		return $this->latitude;
-	}
+    public function getLatitude() {
+        return $this->latitude;
+    }
 
-	public function getLongitude() {
-		return $this->longitude;
-	}
+    public function getLongitude() {
+        return $this->longitude;
+    }
 
-	public function setLatitude($latitude) {
-		$this->latitude = $latitude;
-	}
+    public function setLatitude($latitude) {
+        $this->latitude = $latitude;
+    }
 
-	public function setLongitude($longitude) {
-		$this->longitude = $longitude;
-	}
+    public function setLongitude($longitude) {
+        $this->longitude = $longitude;
+    }
 }
