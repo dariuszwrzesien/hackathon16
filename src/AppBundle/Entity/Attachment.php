8e5242255c988cc6359b350433f1786a5b199c8c
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2016-06-26 05:27:54 +0200

    Added attachments

--- src/AppBundle/Entity/Attachment.php
+++ src/AppBundle/Entity/Attachment.php
@@ -8,6 +8,8 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  */
 class Attachment {
+    const PATH = 'upload/attachment';
+
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id
@@ -24,5 +26,63 @@ class Attachment {
     /**
      * @ORM\Column(type="string", length=250)
      */
-    public $path;
+    public $fileName;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fileName
+     *
+     * @param string $fileName
+     *
+     * @return Attachment
+     */
+    public function setFileName($fileName)
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    /**
+     * Get fileName
+     *
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * Set ticket
+     *
+     * @param \AppBundle\Entity\Ticket $ticket
+     *
+     * @return Attachment
+     */
+    public function setTicket(\AppBundle\Entity\Ticket $ticket = null)
+    {
+        $this->ticket = $ticket;
+
+        return $this;
+    }
+
+    /**
+     * Get ticket
+     *
+     * @return \AppBundle\Entity\Ticket
+     */
+    public function getTicket()
+    {
+        return $this->ticket;
+    }
 }
