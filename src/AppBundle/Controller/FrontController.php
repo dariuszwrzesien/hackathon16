1daf4e0ed7ddffa5318ac3d8d326fbc61e455795
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2016-10-08 14:18:01 +0200

    Change front controller

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace AppBundle\Controller\Site;
+namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class FrontController extends Controller
 {
     /**
      * @Route("/", name="homepage")
