8f6f7abb4af17827d8b9a0e0a755709e4207fb6d
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2016-10-08 13:16:08 +0200

    added comments list

---
+++ src/AdminBundle/Controller/CommentsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CommentsController extends Controller
+{
+    /**
+     * @Route("/admin/comments/{id}", requirements={"id" = "\d+"}, name="adminComments")
+     */
+    public function indexAction($id)
+    {
+        var_dump($id);
+        return $this->render('admin/comments/index.html.twig');
+    }
+}
