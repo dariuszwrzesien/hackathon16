c5ccb45585e364c12d79dd8d13efba2d4aa5c4d5
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2016-10-08 11:55:52 +0200

    Fix routing minor issue

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -26,7 +26,7 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/admin/tickets", name="admin")
+     * @Route("/admin/tickets", name="adminTickets")
      */
     public function ticketsAction()
     {
