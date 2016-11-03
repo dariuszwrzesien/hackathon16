1ee3bcb2031a305b38f5caeaef1fd416735fd144
tarnas14 <tarnas14@gmail.com>
2016-11-03 21:07:20 +0100

    removed the interactive parameters.yml generation and added a developer parameters.yml as default part of init script

--- docker/scripts/init.sh
+++ docker/scripts/init.sh
@@ -1,6 +1,21 @@
 # install composer dependencies
 composer install
 
+# setup development environment parameters
+cat <<EOT > /var/www/symfony/app/config/parameters.yml
+parameters:
+    database_host: mysqldb
+    database_port: null
+    database_name: mydb
+    database_user: user
+    database_password: userpass
+    mailer_transport: smtp
+    mailer_host: localhost
+    mailer_user: null
+    mailer_password: null
+    secret: ThisTokenIsNotSoSecretChangeIt
+EOT
+
 # setting godmode for required folders
 chown -R nobody:nogroup /var/www/symfony/var/cache
 chown -R nobody:nogroup /var/www/symfony/var/logs
